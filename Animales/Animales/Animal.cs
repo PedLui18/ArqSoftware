660b9a54b83dc811a611911c69ea36b9533020d7
PedLui18 <56829253+PedLui18@users.noreply.github.com>
2022-09-05 22:18:46 -0200

    Add files via upload

---
+++ Animales/Animales/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animales
+{
+    public class Animal
+    {
+        public string Caminar()
+        {
+            return "puedo Caminar";
+        }
+        public string Saltar()
+        {
+            return "puedo Saltar";
+        }
+    }
+}
